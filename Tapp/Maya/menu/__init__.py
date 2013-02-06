6549356c521cbd005123dd9967c179eba403934a
Toke Jepsen <tokejepsen@gmail.com>
2013-02-06 11:16:57 +0000

    menu
    menu:
    - restructure code

--- Tapp/Maya/menu/__init__.py
+++ Tapp/Maya/menu/__init__.py
@@ -1,3 +1,15 @@
-import utils
+import maya.cmds as cmds
+import maya.mel as mel
 
-utils.loadMenu()
+#creating the Glapp menu
+gMainWindow = mel.eval('$tmpVar=$gMainWindow')
+
+menuList    = cmds.window(gMainWindow, query=True, menuArray=True)
+if 'tappMenu' in menuList:
+    cmds.deleteUI('tappMenu')
+
+tappMenu=cmds.menu('tappMenu',label='Tapp',parent=gMainWindow,tearOff=True)
+
+#repository menu
+cmd='import Tapp.Maya.utils.repository as mur;repo=mur.Repository();repo.Change()'
+cmds.menuItem(label='Repository Change',parent=tappMenu,command=cmd)
