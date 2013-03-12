6db3afda4a52f352fb1163ca1b046a03a8e30ba3
Toke Jepsen <tokejepsen@gmail.com>
2013-03-12 00:39:15 +0000

    Library wip

--- Tapp/Maya/Library/gui.py
+++ Tapp/Maya/Library/gui.py
@@ -1,31 +1,57 @@
-import os
-import sys
+#import os
+#import sys
 
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-from PyQt4 import uic
-import maya.cmds as cmds
+#from PyQt4 import uic
+#import maya.cmds as cmds
 import maya.OpenMayaUI as omu
 import sip
 
-import Tapp.Maya.Library.config as mlc
+#import Tapp.Maya.Library.config as mlc
 
-uiPath=os.path.dirname(mlc.__file__)+'/ui.ui'
-form,base=uic.loadUiType(uiPath)
+#uiPath=os.path.dirname(mlc.__file__)+'/ui.ui'
+#form,base=uic.loadUiType(uiPath)
 
 # MQtUtil class exists in Maya 2011 and up
 def maya_main_window():
     ptr = omu.MQtUtil.mainWindow()
     return sip.wrapinstance(long(ptr), QtCore.QObject)
 
-class Form(base,form):
+class Form(QtGui.QDialog):
+    
     def __init__(self, parent=maya_main_window()):
         QtGui.QDialog.__init__(self, parent)
-        super(base,self).__init__(parent)
-        self.setupUi(self)
+
+        self.treeView=QtGui.QTreeView(parent=self)
         
         self.setObjectName('tmLibrary')
-        self.treeWidget.addTopLevelItem()
+        
+        self.setFixedHeight(500)
+        self.setFixedWidth(500)
+        
+        self.model = QtGui.QFileSystemModel()
+        
+        root=self.model.setRootPath('c:/temp')
+        
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(root)
+        
+        self.treeView.setHeaderHidden(True)
+        self.treeView.setColumnHidden(1,True)
+        self.treeView.setColumnHidden(2,True)
+        self.treeView.setColumnHidden(3,True)
+        
+        self.treeView.clicked.connect(self.on_treeView_clicked)
+    
+    def on_treeView_clicked(self, index):
+        indexItem = self.model.index(index.row(), 0, index.parent())
+
+        fileName = self.model.fileName(indexItem)
+        filePath = self.model.filePath(indexItem)
+        
+        print fileName
+        print filePath
 
 def show():
     #closing previous dialog
