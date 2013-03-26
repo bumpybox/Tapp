f5a4e9b1bf369c6c9ceb072b808c06858c0caa40
Toke Jepsen <tokejepsen@gmail.com>
2013-03-26 11:11:49 +0000

    install
    all references of name tapp is uppercased to Tapp

--- Tapp/setup/config/tapp_maya.py
+++ Tapp/setup/config/tapp_maya.py
@@ -2,19 +2,19 @@ import maya.cmds as cmds
 import os
 import sys
 
-def tappInstall_UI():
-    if cmds.window('tappInstall_UI', exists=True):
-        cmds.deleteUI('tappInstall_UI')
+def TappInstall_UI():
+    if cmds.window('TappInstall_UI', exists=True):
+        cmds.deleteUI('TappInstall_UI')
     
-    window=cmds.window('tappInstall_UI',title='Tapp Install',w=300,h=110,titleBarMenu=False,sizeable=False)
+    window=cmds.window('TappInstall_UI',title='Tapp Install',w=300,h=110,titleBarMenu=False,sizeable=False)
     
     cmds.columnLayout(w=300,h=110)
     formLayout=cmds.formLayout(w=300,h=110)
     
     text=cmds.text(label='Tapp repository is not found!\nWould you like to locate it?',w=300)
     
-    cancelButton=cmds.button(label='Cancel',w=140,h=50,c=tappInstall_cancel)
-    browseButton=cmds.button(label='Browse',w=140,h=50,c=tappInstall_browse)
+    cancelButton=cmds.button(label='Cancel',w=140,h=50,c=TappInstall_cancel)
+    browseButton=cmds.button(label='Browse',w=140,h=50,c=TappInstall_browse)
     
     cmds.formLayout(formLayout,edit=True,af=[(text,'left',10),(text,'top',10)])
     cmds.formLayout(formLayout,edit=True,af=[(cancelButton,'left',5),(cancelButton,'top',50)])
@@ -22,19 +22,19 @@ def tappInstall_UI():
 
     cmds.showWindow(window)
 
-def tappInstall_browse(*args):
+def TappInstall_browse(*args):
     repoPath=cmds.fileDialog2(dialogStyle=1,fileMode=3)[0]
     
     check=False
     #checking all subdirectories
     for name in os.listdir(repoPath):
         
-        #confirm that this is the tapp directory
+        #confirm that this is the Tapp directory
         if name=='Tapp':
             check=True
     
     if check:
-        #create the text file that contains the tapp directory path
+        #create the text file that contains the Tapp directory path
         path=cmds.internalVar(upd=True)+'Tapp.config'
         
         f=open(path,'w')
@@ -47,15 +47,15 @@ def tappInstall_browse(*args):
         cmds.evalDeferred('import Tapp')
         
         #delete ui
-        cmds.deleteUI('tappInstall_UI')
+        cmds.deleteUI('TappInstall_UI')
         
     else:
         cmds.warning('Selected directory is not the \'Tapp\' directory. Please try again')
 
-def tappInstall_cancel(*args):
-    cmds.deleteUI('tappInstall_UI')
+def TappInstall_cancel(*args):
+    cmds.deleteUI('TappInstall_UI')
 
-def tapp():
+def Tapp():
     path=cmds.internalVar(upd=True)+'Tapp.config'
 
     if os.path.exists(path):
@@ -69,9 +69,9 @@ def tapp():
             #run setup
             cmds.evalDeferred('import Tapp')
         else:
-            tappInstall_UI()
+            TappInstall_UI()
     else:
-        tappInstall_UI()
+        TappInstall_UI()
 
-scriptJobNum=cmds.scriptJob(event=('NewSceneOpened',tapp))
-scriptJobNum=cmds.scriptJob(event=('SceneOpened',tapp))
+scriptJobNum=cmds.scriptJob(event=('NewSceneOpened',Tapp))
+scriptJobNum=cmds.scriptJob(event=('SceneOpened',Tapp))
