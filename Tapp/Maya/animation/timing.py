5534084e89b2ab54987abb5076319638ba67bdbb
Toke Jepsen <tokejepsen@gmail.com>
2013-10-03 13:55:24 +0100

    update timing

--- Tapp/Maya/animation/timing.py
+++ Tapp/Maya/animation/timing.py
@@ -1,6 +1,10 @@
 '''
 slider that is connected to the timeslider
 when scrubbing the slider, record time spend on a frame, ripple scale frame
+bake animation to every keyframe
+    get the scale comparison between original keyframe length (would be contant) and new keyframe length
+    used scale to moved key, 0-1 range is previous key to current key
+    ripple effect the start point for next key
 '''
 
 from PySide import QtGui
