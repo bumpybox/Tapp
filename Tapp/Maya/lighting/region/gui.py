55b2750614699d4ef29424d1a69927764e3bc545
Toke Jepsen <tokejepsen@gmail.com>
2013-10-17 08:33:14 +0100

    region

--- Tapp/Maya/lighting/region/gui.py
+++ Tapp/Maya/lighting/region/gui.py
@@ -6,11 +6,11 @@ import maya.cmds as cmds
 import maya.OpenMayaUI as omui
 
 import Tapp.Maya.lighting.region.resources.region as gui
+reload(gui)
 import Tapp.Maya.lighting.region.utils as utils
 
 '''
 import Tapp.utils.pyside.compileUi as upc
-
 #uiPath=os.path.dirname(__file__)+'/resources/timing.ui'
 uiPath=r'C:\Users\tokejepsen\Documents\GitHub\Tapp\Tapp\Maya\lighting\region\resources\region.ui'
 upc.compileUi(uiPath)
@@ -36,7 +36,45 @@ class Window(QtGui.QMainWindow,gui.Ui_MainWindow):
     
     def create_connections(self):
         
-        self.getPreviewRegion_pushButton.pressed.connect(self.on_getPreviewRgion_pressed)
+        self.refresh_pushButton.pressed.connect(self.refresh)
+        
+        self.getPreviewRegion_pushButton.pressed.connect(self.on_getPreviewRegion_pressed)
+        
+        self.getObjectRegion_pushButton.pressed.connect(self.on_getObjectRegion_pressed)
+        
+        self.connectArnold_pushButton.pressed.connect(self.on_connectArnold_pressed)
+        self.disconnectArnold_pushButton.pressed.connect(self.on_disconnectArnold_pressed)
+        
+        self.connectPreview_pushButton.pressed.connect(self.on_connectPreview_pressed)
+        self.disconnectPreview_pushButton.pressed.connect(self.on_disconnectPreview_pressed)
+    
+    def refresh(self):
+        
+        print 'refresh'
+    
+    def on_getPreviewRegion_pressed(self):
+        
+        utils.getRegionDraw()
+    
+    def on_getObjectRegion_pressed(self):
+        
+        utils.getMeshAnimation()
+    
+    def on_connectArnold_pressed(self):
+        
+        utils.connectArnold()
+    
+    def on_disconnectArnold_pressed(self):
+        
+        utils.disconnectArnold()
+    
+    def on_connectPreview_pressed(self):
+        
+        utils.connectPreview()
+    
+    def on_disconnectPreview_pressed(self):
+        
+        utils.disconnectPreview()
 
 win=Window()
 win.show()
--- Tapp/Maya/lighting/region/resources/region.py
+++ Tapp/Maya/lighting/region/resources/region.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'C:\Users\tokejepsen\Documents\GitHub\Tapp\Tapp\Maya\lighting\region\resources\region.ui'
 #
-# Created: Wed Oct 16 08:25:24 2013
+# Created: Thu Oct 17 08:31:56 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,28 +12,41 @@ from PySide import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(562, 209)
+        MainWindow.resize(562, 259)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
+        self.refresh_pushButton = QtGui.QPushButton(self.centralwidget)
+        self.refresh_pushButton.setMinimumSize(QtCore.QSize(0, 0))
+        self.refresh_pushButton.setObjectName("refresh_pushButton")
+        self.verticalLayout.addWidget(self.refresh_pushButton)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.line_2 = QtGui.QFrame(self.centralwidget)
+        self.line_2.setFrameShape(QtGui.QFrame.HLine)
+        self.line_2.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.verticalLayout.addWidget(self.line_2)
+        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem1)
         self.getPreviewRegion_pushButton = QtGui.QPushButton(self.centralwidget)
         self.getPreviewRegion_pushButton.setObjectName("getPreviewRegion_pushButton")
         self.verticalLayout.addWidget(self.getPreviewRegion_pushButton)
         self.getObjectRegion_pushButton = QtGui.QPushButton(self.centralwidget)
         self.getObjectRegion_pushButton.setObjectName("getObjectRegion_pushButton")
         self.verticalLayout.addWidget(self.getObjectRegion_pushButton)
-        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem)
+        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem2)
         self.line = QtGui.QFrame(self.centralwidget)
         self.line.setFrameShape(QtGui.QFrame.HLine)
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
         self.line.setObjectName("line")
         self.verticalLayout.addWidget(self.line)
-        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem1)
+        spacerItem3 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem3)
         self.connectPreview_pushButton = QtGui.QPushButton(self.centralwidget)
         self.connectPreview_pushButton.setObjectName("connectPreview_pushButton")
         self.verticalLayout.addWidget(self.connectPreview_pushButton)
@@ -62,6 +75,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.refresh_pushButton.setText(QtGui.QApplication.translate("MainWindow", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
         self.getPreviewRegion_pushButton.setText(QtGui.QApplication.translate("MainWindow", "Get Preview Region", None, QtGui.QApplication.UnicodeUTF8))
         self.getObjectRegion_pushButton.setText(QtGui.QApplication.translate("MainWindow", "Get Object Region", None, QtGui.QApplication.UnicodeUTF8))
         self.connectPreview_pushButton.setText(QtGui.QApplication.translate("MainWindow", "Connect Preview", None, QtGui.QApplication.UnicodeUTF8))
