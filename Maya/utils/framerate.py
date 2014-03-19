df5f22189e7bfd900b8d130f13d6a3cbbb7d1d9c
Toke Jepsen <tokejepsen@gmail.com>
2014-03-19 12:19:39 +0000

    framerate formatting

--- Maya/utils/framerate.py
+++ Maya/utils/framerate.py
@@ -1,17 +1,18 @@
 import maya.cmds as cmds
 
+
 def FrameratePrompt():
-    
-    framerate=cmds.currentUnit(q=True,time=True)
-    
-    if framerate!='pal':
-        message='Maya session is not working with 25 fps.\n'
-        message+='Do you want to switch to 25 fps?'
-        result=cmds.confirmDialog(title='FRAMERATE',message=message,
-                                  icon='warning',backgroundColor=(0.5,0,0))
-        
-        if result=='Confirm':
-            
+
+    framerate = cmds.currentUnit(q=True, time=True)
+
+    if framerate != 'pal':
+        message = 'Maya session is not working with 25 fps.\n'
+        message += 'Do you want to switch to 25 fps?'
+        result = cmds.confirmDialog(title='FRAMERATE', message=message,
+                                  icon='warning', backgroundColor=(0.5, 0, 0))
+
+        if result == 'Confirm':
+
             cmds.currentUnit(time='pal')
 
-scriptJobNum=cmds.scriptJob(event=('SceneOpened',FrameratePrompt))
+scriptJobNum = cmds.scriptJob(event=('SceneOpened', FrameratePrompt))
