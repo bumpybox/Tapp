2ae28503ac8006417a5a8b8f194640010a3dadbc
Toke Jepsen <tokejepsen@gmail.com>
2014-05-27 22:19:21 +0100

    Maya: menu
    removed settings till its sorted

--- Maya/menu.py
+++ Maya/menu.py
@@ -4,16 +4,13 @@ import maya.mel as mel
 #creating the Tapp menu
 gMainWindow = mel.eval('$tmpVar=$gMainWindow')
 
-menuList    = cmds.window(gMainWindow, query=True, menuArray=True)
+menuList = cmds.window(gMainWindow, query=True, menuArray=True)
 if 'tappMenu' in menuList:
     cmds.deleteUI('tappMenu')
 
-tappMenu=cmds.menu('tappMenu',label='Tapp',parent=gMainWindow,tearOff=True)
+tappMenu = cmds.menu('tappMenu', label='Tapp', parent=gMainWindow,
+                     tearOff=True)
 
 #tapp menu
-cmd='import Tapp.Maya.gui as gui;reload(gui);win=gui.Window();win.show()'
-cmds.menuItem(label='Tapp Tools',parent=tappMenu,command=cmd)
-
-#settings menu
-cmd='import Tapp.Maya.settings.gui as win;reload(win);win.show()'
-cmds.menuItem(label='Settings',parent=tappMenu,command=cmd)
+cmd = 'import Tapp.Maya.gui as gui;reload(gui);win=gui.Window();win.show()'
+cmds.menuItem(label='Tapp Tools', parent=tappMenu, command=cmd)
